2116d5d3696d7889dcf0d152037b93c33f813d32
Cristal De Jesus <cristaledith350@gmail.com>
2026-05-17 18:51:35 -0700

    Small change

--- src/app/root.ts
+++ src/app/root.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root-shell',
+  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   template: '<router-outlet></router-outlet>'
